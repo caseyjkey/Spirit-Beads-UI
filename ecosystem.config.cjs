89f521eea96e5dea3b4ed1b3b971fdfafb16530e
Casey <hey@keycasey.com>
2026-01-08 21:10:43 -0800

    prod ecosystem serve built files

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -9,9 +9,6 @@ module.exports = {
     watch: false,
     max_memory_restart: '1G',
     env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
       NODE_ENV: 'production'
     },
     log_file: '/var/www/spirit-bead-designs/logs/combined.log',
